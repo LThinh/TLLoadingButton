386cf847816046780067205b213212abf99971f8
thinhle <thinhlefrs@gmail.com>
2020-04-11 10:14:41 +0700

    Set up initial project

---
+++ TLLoadingButton/TLLoadingButton.h
@@ -0,0 +1,19 @@
+//
+//  TLLoadingButton.h
+//  TLLoadingButton
+//
+//  Created by ThinhLe on 4/11/20.
+//  Copyright © 2020 ThinhLe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TLLoadingButton.
+FOUNDATION_EXPORT double TLLoadingButtonVersionNumber;
+
+//! Project version string for TLLoadingButton.
+FOUNDATION_EXPORT const unsigned char TLLoadingButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TLLoadingButton/PublicHeader.h>
+
+
